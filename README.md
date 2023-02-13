cd496d4698e92e41ff78c5a180ada6eff8ef3ed5
Jaydip Godhani <jaydip@dioneapps.com>
2023-02-13 10:21:42 +0530

    Package Added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DACombineAlamofireAPI
+
+A description of this package.
---
+++ Sources/DACombineAlamofireAPI/DACombineAlamofireAPI.swift
@@ -0,0 +1,6 @@
+public struct DACombineAlamofireAPI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
