aa8cc44bf4ad1c99bbaa3e58c045f6997fd5cc86
Tushar Moradiya <jaydip@dioneapps.com>
2023-11-27 15:43:12 +0530

    Fixed error handling

--- Sources/DACombineAlamofireAPI/DAError.swift
+++ Sources/DACombineAlamofireAPI/DAError.swift
@@ -39,3 +39,13 @@ public enum DAError: LocalizedError {
         }
     }
 }
+
+public class DAErrorModel {
+    let status : DAError
+    let message : String
+    
+    init(status: DAError, message: String) {
+        self.status = status
+        self.message = message
+    }
+}
